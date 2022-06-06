71423e21220086e9d18b3cd07951c0b598730a86
0618 <o.6180339@gmail.com>
2022-06-06 10:02:58 -0700

    feat(docs): generate allTyleFIlesData

--- docs/scripts/generate-props-tables.ts
+++ docs/scripts/generate-props-tables.ts
@@ -2,8 +2,6 @@ import fs from 'fs';
 import path from 'path';
 import { Node, Project, Symbol, Type, VariableDeclaration } from 'ts-morph';
 
-console.log('__dirname: ', __dirname);
-
 const project = new Project({
   tsConfigFilePath: path.resolve(
     __dirname,
@@ -11,57 +9,49 @@ const project = new Project({
   ),
 });
 
-// console.log(project);
+const allTypeFilesData = new Map();
 
 const source = project.getSourceFile(
   path.resolve(__dirname, '../../packages/react/src/primitives/types/index.ts')
 );
 
-// console.log('path: ', source.getReferencedSourceFiles()[0].getInterfaces());
-
 const allTypeFiles = source.getReferencedSourceFiles();
 
 allTypeFiles.forEach((typeFile) => {
-  console.log('🏠 Path: ', typeFile.getBaseName());
-  console.log('🐛', typeFile.getTypeAliases()[0]?.getText());
+  const typeFileName = typeFile
+    .getBaseName()
+    .slice(0, typeFile.getBaseName().indexOf('.ts'));
+  const typeFileData = new Map();
   const typeAliases = typeFile.getTypeAliases();
   if (typeAliases) {
     typeAliases.forEach((typeAlias) => {
+      const typeAliasData = new Map();
       const typeAliasJsDocs = typeAlias.getJsDocs();
       const typeAliasDescription = typeAliasJsDocs[0]?.getDescription() ?? '';
       const typeAliasName = typeAlias.getNameNode().getText();
       const typeAliasType = typeAlias.getTypeNode().getText();
-      console.log('🍎 typeAlias with JS Docs =>');
-      console.log('typeAliasDescription:', typeAliasDescription);
-      console.log('typeAliasName:', typeAliasName);
-      console.log('typeAliasType:', typeAliasType);
+      typeAliasData.set('name', typeAliasName);
+      typeAliasData.set('type', typeAliasType);
+      typeAliasData.set('description', typeAliasDescription);
+      typeFileData.set(typeAliasName, typeAliasData);
     });
   }
 
   typeFile.getInterfaces().forEach((typeInterface) => {
     typeInterface.getProperties().forEach((typeProperty) => {
+      const typeInterfaceData = new Map();
       const propertyJsDocs = typeProperty.getJsDocs()[0];
       const propertyDescription = propertyJsDocs?.getDescription() ?? '';
       const propertyName = typeProperty.getNameNode().getText();
       const PropertyType = typeProperty.getTypeNode().getText();
-      console.log('😁 interface =>');
-      console.log('Description:', propertyDescription);
-      console.log('Name:', propertyName);
-      console.log('Type:', PropertyType);
+      typeInterfaceData.set('name', propertyName);
+      typeInterfaceData.set('type', PropertyType);
+      typeInterfaceData.set('description', propertyDescription);
+      typeFileData.set(propertyName, typeInterfaceData);
     });
   });
+
+  allTypeFilesData.set(typeFileName, typeFileData);
 });
 
-// const properties = source.getInterfaces()[0].getProperties();
-// const properties = allTypeFiles[0].getInterfaces()[0].getProperties();
-
-// properties.forEach((property) => {
-//   const propertyJsDocs = property.getJsDocs()[0];
-//   const propertyDescription = propertyJsDocs.getDescription();
-//   const propertyName = property.getNameNode().getText();
-//   const PropertyType = property.getTypeNode().getText();
-//   console.log('😁');
-//   console.log('Description:', propertyDescription);
-//   console.log('Name:', propertyName);
-//   console.log('Type:', PropertyType);
-// });
+console.log(allTypeFilesData);
